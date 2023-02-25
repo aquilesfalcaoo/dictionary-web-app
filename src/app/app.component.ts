72147f7ece69d78f68a971a6ec8d996831c971fb
Aquiles <aquilesfalcaoo@gmail.com>
2023-02-25 10:43:10 -0300

    feat: realizado o subscribe e unsubscribe da palavra pesquisada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,44 @@
 import { DictionaryService } from './service/dictionary.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  form = new FormGroup({
+export class AppComponent implements OnDestroy {
+  public subscription: Subscription = new Subscription();
+  public form = new FormGroup({
     word: new FormControl('', Validators.required),
   });
 
   constructor(private service: DictionaryService) {}
 
-  showDropdown = () => {
+  public showDropdown = () => {
     const dropdown = document.querySelector('.dropdown-content');
     dropdown?.classList.toggle('active');
   };
 
-  searchWord = () => {
+  public toggleTheme = () => {
+    const body = document.querySelector('body');
+    body?.classList.toggle('black-theme');
+  };
+
+  public searchWord = () => {
     const typedWord: any = this.form.get('word')?.value;
-    this.service.search(typedWord).subscribe({
+    this.subscription = this.service.search(typedWord).subscribe({
       next: (res) => {
         console.log(res);
       },
-      error: (errorMessage: HttpErrorResponse) => console.error(errorMessage),
+      error: (error: HttpErrorResponse) => console.error(error),
       complete: () => console.log('Observable completado.'),
     });
   };
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
